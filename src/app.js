bb458047321e58e2c65f2a5ac6cf400e9e3e81e5
Windless95 <barrerajoelj@gmail.com>
2021-09-04 00:14:46 +0000

    visiting countdown

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  (function() {
+    const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+    let visit = "Sep 4, 2021 16:00:00",
+      countDown = new Date(visit).getTime(),
+      x = setInterval(function() {
+        let now = new Date().getTime(),
+          distance = countDown - now;
+
+        (document.getElementById("days").innerText = Math.floor(
+          distance / day
+        )),
+          (document.getElementById("hours").innerText = Math.floor(
+            (distance % day) / hour
+          )),
+          (document.getElementById("minutes").innerText = Math.floor(
+            (distance % hour) / minute
+          )),
+          (document.getElementById("seconds").innerText = Math.floor(
+            (distance % minute) / second
+          ));
+        //seconds
+      }, 0);
+  })();
 };
